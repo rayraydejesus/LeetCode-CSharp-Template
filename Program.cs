56708f5f0e48dbfe0c424a140b0b37c9f81626f3
RYPTAR <ray.dej21@gmail.com>
2023-09-20 15:30:36 -0400

    Built basic console app framework

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the leetcode console app!");
 
+        string input = "";
+        int response = -1;
+        bool running = true;
+        
+        while (running)
+        {
+            Console.WriteLine("Please input the number of the question you'd like to see.");
+            input = Console.ReadLine();
+
+            if(int.TryParse(input, out response))
+            {
+                Console.WriteLine("Leetcode rulez!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid response.");
+            }
+
+
+            Console.WriteLine("Would you like to try again? (Y/N)");
+            input = Console.ReadLine().ToLower();
+
+            if (input.Length < 1)
+                break;
+            else if (input[0] == 'y')
+                continue;
+            else
+                break;
+        }
+
+        Console.WriteLine("Goodbye!");
+    }
+}
