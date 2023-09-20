786196b52577f307a4294d6241da95afea8fdda2
RYPTAR <ray.dej21@gmail.com>
2023-09-20 16:16:06 -0400

    created leetcode problem abstract and interface

---
+++ Common/ILeetCodeProblem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Leetcode_CSharp_Template.Common
+{
+	public interface ILeetCodeProblem
+	{
+		public object parseInput(string input);
+		public bool runSolution(object input);
+	}
+}
+
---
+++ Common/LeetCodeProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Leetcode_CSharp_Template.Common
+{
+	public abstract class LeetCodeProblem : ILeetCodeProblem
+	{
+		public readonly int key;
+		public readonly string description;
+		public readonly string url;
+
+		public abstract object parseInput(string input);
+		public abstract bool runSolution(object input);
+	}
+}
+
